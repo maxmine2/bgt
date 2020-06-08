71acdb7df892229e4cb5e1932b2ba0f7240ebc18
maxmine2 <mbudko2@gmail.com>
2020-06-08 16:04:42 +0700

    Start of logging work

--- man.py
+++ man.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import pstg
 import json
+import logging as log
+
+log.basicConfig(filename='man.log', filemode='w', format='%(levelname)s - %(name)s - %(message)s')
 
 man_settings = json.load(open("man_set.json", "r"))
 
@@ -17,6 +20,7 @@ BUG_STATUS = {
 
 @app.route('/')
 def index():
+    log.info('Asked for Index, returned succesfull')
     return render_template("index.html", project_name=man_settings["project_name"])
 
 @app.route('/bug')
@@ -24,21 +28,30 @@ def index():
 def all_bug():
     bugs = []
     for every_bug in pstg.get_all_bugs():
+        log.debug(f"Appending element {(every_bug[0], every_bug[2])}")
         bugs.append((every_bug[0], every_bug[2]))
+    log.info('Asked for Bugs Page, parsing sucessfull, returning sucessful')
     return render_template("all_bugs.html", bugs=bugs)
 
 @app.route('/bug/create', methods=['post', 'get'])
 def bug_create_page():
     msg = ''
     bug_id_new = 0
+    log.info("Asked for Create Bug Page")
     if request.method == "POST":
+        log.debug("Method POST used")
         bug_id_new = pstg.create_new_bug({"t": request.form.get('title'), "m": request.form.get('mail'), "d": request.form.get('description'), "p": request.form.get('pass'),})
-        msg = f'Bug succesfully created. '
+        log.debug(f"Bug Id returned: {bug_id_new}")
+        msg = 'Bug succesfully created. '
+        log.info(f'Bug #{bug_id_new} created sucessfull')
+    log.info('Returning sucessfull')
     return render_template("bc.html", message=msg, link= f"/bug/{bug_id_new}")
 
 @app.route('/bug/<int:bug_id>')
 def bug_id_page(bug_id):
+    log.info("Asked for Bug Info Page")
     bug = pstg.get_bug_by_id(bug_id)
+    log.debug(f"Collected Bug's id:{bug[0][0]}")
     if bug == []:
         return render_template("bug_id.html", bug_id=bug_id, bug_title="Bug not found")
     return render_template("bug_id.html", bug_id=bug_id, bug_title=bug[0][2], mail=bug[0][4], bug_description=bug[0][3], bug_status_pbl=BUG_STATUS[bug[0][1]])
@@ -100,80 +113,80 @@ def bug_cancel(bug_id):
 
 @app.errorhandler(400)
 @app.route('/err400')
-def e400(e):
+def e400():
     return render_template('400.html'), 400
 @app.errorhandler(401)
 @app.route('/err401')
-def e401(e):
+def e401():
     return render_template('401.html'), 401
 @app.route('/err402')
-def e402(e):
+def e402():
     return render_template('402.html'), 402
 @app.errorhandler(403)
 @app.route('/err403')
-def e403(e):
+def e403():
     return render_template('403.html'), 403
 @app.errorhandler(404)
 @app.route('/err404')
-def e404(e):
+def e404():
     return render_template('404.html'), 404
 @app.errorhandler(405)
 @app.route('/err405')
-def e405(e):
+def e405():
     return render_template('405.html'), 405
 @app.errorhandler(406)
 @app.route('/err406')
-def e406(e):
+def e406():
     return render_template('406.html'), 406
 @app.route('/err407')
-def e407(e):
-    return render_template('407.html'), 407
+def e407():
+    return render_template('407.html')  
 @app.errorhandler(408)
 @app.route('/err408')
-def e408(e):
-    return render_template('408.html'), 408
+def e408():
+    return render_template('408.html')
 @app.errorhandler(409)
 @app.route('/err409')
-def e409(e):
+def e409():
     return render_template('409.html'), 409
 @app.errorhandler(410)
 @app.route('/err410')
-def e410(e):
+def e410():
     return render_template('410.html'), 410
 @app.errorhandler(411)
 @app.route('/err411')
-def e411(e):
+def e411():
     return render_template('411.html'), 411
 @app.errorhandler(412)
 @app.route('/err412')
-def e412(e):
+def e412():
     return render_template('412.html'), 412
 @app.errorhandler(413)
 @app.route('/err413')
-def e413(e):
+def e413():
     return render_template('413.html'), 413
 @app.errorhandler(414)
 @app.route('/err414')
-def e414(e):
+def e414():
     return render_template('414.html'), 414
 @app.errorhandler(415)
 @app.route('/err415')
-def e415(e):
+def e415():
     return render_template('415.html'), 415
 @app.errorhandler(416)
 @app.route('/err416')
-def e416(e):
+def e416():
     return render_template('416.html'), 416
 @app.errorhandler(417)
 @app.route('/err417')
-def e417(e):
+def e417():
     return render_template('417.html'), 417
 @app.errorhandler(418)
 @app.route('/err418')
 @app.route('/errTeapod')
 @app.route('/teapod')
 @app.route('/iamteapod')
-def e418(e):
+def e418():
     return render_template('418.html'), 418
       
 if __name__ == "__main__":
